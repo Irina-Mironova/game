267a7d6b1b56356f3c3ee59abd6ad6aae3238e0a
IrinaMironova <irina250282@mail.ru>
2021-07-29 00:17:54 +0500

    HomeWork 1

--- core/src/ru/game/StarGame.java
+++ core/src/ru/game/StarGame.java
@@ -13,14 +13,14 @@ public class StarGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("background.png");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
