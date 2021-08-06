dcc99dca01cdbbad6c62c0da49318e67f43aa2f1
IrinaMironova <irina250282@mail.ru>
2021-08-06 17:48:07 +0500

    HomeWork 3

---
+++ core/src/ru/game/sprite/PlayButton.java
@@ -0,0 +1,44 @@
+package ru.game.sprite;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.game.base.BaseButton;
+import ru.game.math.Rect;
+import ru.game.screen.GameScreen;
+
+public class PlayButton extends BaseButton {
+
+    private static final float PADDING = 0.03f;
+    private final Game game;
+
+
+
+    public PlayButton(TextureAtlas atlas, Game game) {
+        super(atlas.findRegion("btPlay"));
+        this.game = game;
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(0.23f);
+        setBottom(worldBounds.getBottom() + PADDING);
+        setLeft(worldBounds.getLeft() + PADDING);
+    }
+
+    @Override
+    public void action() {
+      game.setScreen(new GameScreen());
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        return super.touchUp(touch, pointer, button);
+    }
+}
